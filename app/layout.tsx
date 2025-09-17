ab3d07a57ae51d71199b4e6794560ca3f8abc3c1
SYSFOC <153262529+sysfocdev@users.noreply.github.com>
2025-09-17 15:10:53 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800" "900"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   display: "swap",
 });
 
